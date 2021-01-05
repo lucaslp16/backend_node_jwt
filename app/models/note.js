e1c11125b05418c4f19a1419a449206b07c644f2
lucas <lucasperosini@hotmail.com>
2021-01-05 01:19:23 -0300

    created schema + connection with mongodb

---
+++ app/models/note.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+let noteSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    created_at: { type: Date, default: Date.now},
+    update_at: { type: Date, default: Date.now },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema);
---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require ('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/javascriptNode',{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=>{
+    console.log('Connection sucessful')
+}).catch((err)=>{
+    console.log (err);
+})
