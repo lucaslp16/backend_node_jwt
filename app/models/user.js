f370ab9ac494da2e588d4d6f5c0a75dfe3d9146d
lucas <lucasperosini@hotmail.com>
2021-01-05 02:58:54 -0300

    Created router /login with authentication JWT

--- app/models/user.js
+++ app/models/user.js
@@ -25,7 +25,16 @@ userSchema.pre('save', function(next){
             }    
         )
     }
-})
+});
+
+userSchema.methods.isCorrectPassword = function (password, callback){
+    bcrypt.compare(password, this.password, function(err, same){
+        if(err)
+            callback(err);
+        else
+            callback(err,same)
+    })
+}
 
 
 module.exports = mongoose.model ('User', userSchema); 
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const User = require ('../models/user');
+const jwt = require ('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.JWT_TOKEN;
+
 
 router.post ('/register',  async(req,res)=>{
   const { name, email,password} = req.body;
@@ -13,6 +17,30 @@ router.post ('/register',  async(req,res)=>{
     res.status(500).json({error: 'Error'}); 
   }
 
+});
+
+router.post('/login', async(req,res)=>{
+  const {email, password} = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if(!user)
+      res.status(401).json({error: 'Incorrect email or password'});
+      else{
+        user.isCorrectPassword(password, function(err, same){
+          if(!same)
+          res.status(401).json({error: 'Incorrect email or password'});
+          else{
+            const token = jwt.sign({email}, secret,{ expiresIn: '1d'})
+            res.json({user: user, token: token});
+          }
+        })
+      }
+  } catch (error) {
+    res.status(500).json({error: 'Error, please try again'})
+  }
+//
 })
+
 
 module.exports = router;
